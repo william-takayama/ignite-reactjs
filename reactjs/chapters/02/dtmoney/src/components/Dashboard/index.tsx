a4720b5b0d8da36cfa1c8ff4c1aa214a7f0bef77
William Takayama <william-sobreira@hotmail.com>
2021-07-17 21:09:37 -0300

    feat: use basic Loader component

--- reactjs/chapters/02/dtmoney/src/components/Dashboard/index.tsx
+++ reactjs/chapters/02/dtmoney/src/components/Dashboard/index.tsx
@@ -1,10 +1,15 @@
+import { useTransactions } from "../../core/hooks/useTransactions";
+import { Loader } from "../Loader";
 import { Summary } from "../Summary";
 import { TransactionTable } from "../TransactionTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
+  const { isLoading } = useTransactions();
+
   return (
     <Container>
+      <Loader isLoading={isLoading} />
       <Summary />
       <TransactionTable />
     </Container>
