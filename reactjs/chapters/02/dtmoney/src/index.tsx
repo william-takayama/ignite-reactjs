2daeeffce3752d737788c001eaf2dc017617d2f8
William Takayama <william-sobreira@hotmail.com>
2021-07-17 21:10:09 -0300

    feat: mock basic db with inital data and 2 endpoints using MirageJS

--- reactjs/chapters/02/dtmoney/src/index.tsx
+++ reactjs/chapters/02/dtmoney/src/index.tsx
@@ -1,6 +1,50 @@
+import { createServer, Model } from "miragejs";
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelancer",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-06-20 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Rental",
+          type: "withdraw",
+          category: "Home",
+          amount: 1500,
+          createdAt: new Date("2021-07-15 09:00:00"),
+        },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transaction", data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
