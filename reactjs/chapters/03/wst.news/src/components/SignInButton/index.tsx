ed4ca62f6527667bf5b6a212eeafb6cf25a8a359
William Takayama <william-sobreira@hotmail.com>
2021-07-24 20:39:06 -0300

    feat: next-auth with OAUTH for github

--- reactjs/chapters/03/wst.news/src/components/SignInButton/index.tsx
+++ reactjs/chapters/03/wst.news/src/components/SignInButton/index.tsx
@@ -1,24 +1,36 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
+
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
+
+  console.log(session);
 
   return (
-    <button type="button" className={styles.signInButton}>
-      {isUserLoggedIn ? (
-        <>
+    <>
+      {session ? (
+        <button
+          type="button"
+          className={styles.signInButton}
+          onClick={() => signOut()}
+        >
           <FaGithub color="#04d361" />
-          Will
+          {session.user?.name}
           <FiX color="#737380" className={styles.closeIcon} />
-        </>
+        </button>
       ) : (
-        <>
+        <button
+          type="button"
+          className={styles.signInButton}
+          onClick={() => signIn("github")}
+        >
           <FaGithub color="#eba417" />
           Sign in with Github
-        </>
+        </button>
       )}
-    </button>
+    </>
   );
 }
