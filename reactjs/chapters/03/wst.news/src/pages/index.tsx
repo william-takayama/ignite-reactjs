b92d2588295020d10a157516650560549e6d8dfa
William Takayama <william-sobreira@hotmail.com>
2021-07-24 20:38:45 -0300

    feat: integrating with stripe payment API

--- reactjs/chapters/03/wst.news/src/pages/index.tsx
+++ reactjs/chapters/03/wst.news/src/pages/index.tsx
@@ -1,5 +1,38 @@
+import { GetStaticProps } from "next";
 import { HomePageComponent } from "../components/PageComponents/Home";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
-  return <HomePageComponent />;
+export declare type Product = {
+  priceId: string;
+  priceAmount: number;
+};
+interface HomeProps {
+  product: Product;
 }
+
+export default function Home({ product }: HomeProps) {
+  return <HomePageComponent product={product} />;
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const price = await stripe.prices.retrieve("price_1JGnB2DTe91J19Ucatta5YkA", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    priceAmount: price.unit_amount
+      ? new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(price.unit_amount / 100)
+      : 0,
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
