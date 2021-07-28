d66ea77ad7933614063a9b241b8587415a6d6fc9
William Takayama <william-sobreira@hotmail.com>
2021-07-28 17:33:18 -0300

    feat: nextauth implementation - checking if there's a user already created

--- reactjs/chapters/03/wst.news/src/pages/api/auth/[...nextauth].ts
+++ reactjs/chapters/03/wst.news/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentications providers
@@ -11,4 +15,34 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
+  jwt: {
+    // node-jose-tools - jose newkey -s 256 -t oct -a HS512
+    secret: process.env.SIGNIN_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            // condition
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email ?? ""))
+              )
+            ),
+            // condition true
+            q.Create(q.Collection("users"), { data: { email } }),
+            // else select
+            q.Get(q.Match("user_by_email", q.Casefold(user.email ?? "")))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
