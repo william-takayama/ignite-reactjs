c2312e0c41b58c1d33df39a0586df978a0c7880d
William Takayama <william-sobreira@hotmail.com>
2021-07-31 17:42:02 -0300

    update: put on right scope

--- reactjs/chapters/03/wst.news/src/services/stripe-js.ts
+++ reactjs/chapters/03/wst.news/src/services/stripe-js.ts
@@ -1,11 +1,11 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-if (process.env.NEXT_PUBLIC_STRIPE_API_KEY == null) {
-  throw new Error("NEXT_PUBLIC_STRIPE_API_KEY is missing");
-}
-
 export async function getStripeJs() {
-  const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_API_KEY!);
+  if (process.env.NEXT_PUBLIC_STRIPE_API_KEY == null) {
+    throw new Error("NEXT_PUBLIC_STRIPE_API_KEY is missing");
+  }
+
+  const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_API_KEY);
 
   return stripeJs;
 }
